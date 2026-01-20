4cc641f6af1f6e8b9e7dacc917e2529342c4ea0b
pratham5816 <prathamtaheem@gmail.com>
2026-01-20 13:15:10 +0530

    Entities added to model

---
+++ back_end/VehicleTracker/src/main/java/com/model/Vehicle.java
@@ -0,0 +1,25 @@
+package com.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Vehicle {
+    @Id
+    @Column(length = 30)
+    private String vechicleNumber;
+    @Column(length = 30)
+    private String vehicleType;
+    private Integer challan_Exp;   // expenditure
+    private Integer vehicle_Exp;
+
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user")
+    private User user;
+}
